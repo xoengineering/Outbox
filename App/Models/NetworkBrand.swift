a77e8797dc0b96d0e8175c56e34e9cc279088cd8
Shane Becker <veganstraightedge@gmail.com>
2026-07-31 16:17:10 -0700

    Fix Threads brand fill washing out: concrete adaptive colors instead of hierarchical styles

--- App/Models/NetworkBrand.swift
+++ App/Models/NetworkBrand.swift
@@ -4,20 +4,28 @@ import SwiftUI
 extension Network {
   /// The official brand color.
   ///
-  /// Threads' is black, expressed as `.primary` so it flips with the color scheme.
+  /// Threads' is black, expressed as a concrete adaptive color so it flips with
+  /// the color scheme. (Hierarchical styles like `.primary` render as vibrant
+  /// materials in some contexts, washing out fills.)
   var brandColor: AnyShapeStyle {
     switch self {
     case .bluesky: AnyShapeStyle(Color(red: 0x11 / 255, green: 0x85 / 255, blue: 0xFE / 255))
     case .mastodon: AnyShapeStyle(Color(red: 0x63 / 255, green: 0x64 / 255, blue: 0xFF / 255))
-    case .threads: AnyShapeStyle(.primary)
+    case .threads: AnyShapeStyle(Color.primary)
     }
   }
 
   /// A legible content color on top of `brandColor`.
   var brandContrastColor: AnyShapeStyle {
     switch self {
-    case .bluesky, .mastodon: AnyShapeStyle(.white)
-    case .threads: AnyShapeStyle(.background)
+    case .bluesky, .mastodon:
+      AnyShapeStyle(.white)
+    case .threads:
+      #if os(macOS)
+        AnyShapeStyle(Color(nsColor: .windowBackgroundColor))
+      #else
+        AnyShapeStyle(Color(uiColor: .systemBackground))
+      #endif
     }
   }
 }
